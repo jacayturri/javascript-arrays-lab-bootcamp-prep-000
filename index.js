0cb095acc6577bff3cfff3c5230a5f836e8daa1c
Jeremiah  Acayturri <jtacayturri@gmail.com>
2018-12-31 03:11:51 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,3 +1,31 @@
-var kittens = [] //define your array here
+var kittens = ["Milo", "Otis", "Garfield"]
 
-// Add your functions and code here
+function destructivelyAppendKitten(name) {
+  kittens.push(name);
+  return kittens;
+}
+
+function destructivelyPrependKitten(name) {
+  kittens.unshift(name);
+  return kittens;
+}
+
+function destructivelyRemoveLastKitten() {
+  kittens.pop();
+  return kittens;
+}
+
+function destructivelyRemoveFirstKitten() {
+  kittens.shift();
+  return kittens;
+}
+
+function appendKitten(name) {
+  const newKitten = kittens.concat(name);
+  return newKitten;
+}
+
+function prependKitten(name) {
+  const newLastKitten = appendKitten(name); 
+  return newLastKitten;
+}
