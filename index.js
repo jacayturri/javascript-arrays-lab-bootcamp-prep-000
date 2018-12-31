a8c57db3d76cfc7bf10f67b05b763ea0fad07ff8
Jeremiah  Acayturri <jtacayturri@gmail.com>
2018-12-31 03:21:31 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -26,7 +26,7 @@ function appendKitten(name) {
 }
 
 function prependKitten(name) {
-  const newLastKitten = kittens.concat(name,...kittens)
+  const newLastKitten = kittens.concat(name,...kittens);
   return newLastKitten;
 }
 
